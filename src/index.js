4bb451d8dd128eef83f9ec217698943698e16ab7
AntLafI <antoine.lafitte@etu.univ-amu.fr>
2023-03-13 11:33:21 +0100

    Structure app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-//to do list
+var tasks = [{"title":"1.Idée","isChecked":true},{"title":"2.Marché","isChecked":true},{"title":"3.Wireframe","isChecked":true},{"title":"4.Design","isChecked":true},{"title":"5.Landingpage","isChecked":true},{"title":"6.Développement","isChecked":false},{"title":"7.Publish","isChecked":false},{"title":"8.Pub","isChecked":false},{"title":"9.Feedback","isChecked":false}]
+localStorage.setItem('tasks',JSON.stringify(tasks))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class ToDoApp extends React.Component {
+
+
+  render(){
+    <Header />;
+    <ToDoBoard />;
+    <Footer />
+  }
+}
+
+class Header extends React.Component {
+
+}
+
+class ToDoBoard extends React.Component {
+  render () {
+    <Tasks />
+  }
+}
+
+class Footer extends React.Component {
+
+}
+
+class Tasks extends React.Component {
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ToDoApp />);
+
+
