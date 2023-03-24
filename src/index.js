ddc4c65d0418c6814033aaa29fa4ae74571912a6
AntLafI <antoine.lafitte@etu.univ-amu.fr>
2023-03-24 10:59:14 +0100

    removed test tasks

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Popup from 'reactjs-popup';
 import ReactDOM from 'react-dom/client';
 
-var tasks = [{"title":"1.Idée","isChecked":true},{"title":"2.Marché","isChecked":true},{"title":"3.Wireframe","isChecked":true},{"title":"4.Design","isChecked":true},{"title":"5.Landingpage","isChecked":true},{"title":"6.Développement","isChecked":false},{"title":"7.Publish","isChecked":false},{"title":"8.Pub","isChecked":false},{"title":"9.Feedback","isChecked":false}]
-localStorage.setItem('tasks',JSON.stringify(tasks))
-
 
 
 class ToDoApp extends React.Component {
